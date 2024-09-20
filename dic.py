195ff1bb81913add2ea1b3578d0c760ff821c310
GustavSandberg <gusa07002@utb.vaxjo.se>
2024-09-20 09:29:39 +0200

    Create dic.py

---
+++ dic.py
@@ -0,0 +1,12 @@
+records = {
+    "Herzeleid": 1995,
+    "Sehnsucht": 1997,
+    "Mutter": 2001,
+    "Resie": 2004,
+    "Rosenrot": 2005,
+    "Liebe ist für alla de": 2009,
+    "Rammstein": 2019,
+    "Zeit": 2022,
+}
+for album, year in records.items():
+    print(f"{album} släpptes {year}")
